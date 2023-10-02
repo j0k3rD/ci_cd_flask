6c86a589cc45f447f135cc3ca1475ed48df0964d
Aaron <agm.moya@alumno.um.edu.ar>
2023-10-01 21:06:37 -0300

    editing json

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import flask
 app = flask.Flask(__name__)
 
 @app.route('/')
-# devolver 'hola' en json
 def index():
-    return flask.jsonify({'message': 'Hello World! My name is Aaron!!'})
+    return flask.jsonify({'message': 'Hello World! This is a simple API'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')

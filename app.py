636490c28b7b83a01a741ca3928400480354c730
Aaron <agm.moya@alumno.um.edu.ar>
2023-10-03 18:58:08 -0300

    editing json yes, build

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.jsonify({'message': 'Hello World! NOW Deploy!!'})
+    return flask.jsonify({'message': 'Hello World! Build!!'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')

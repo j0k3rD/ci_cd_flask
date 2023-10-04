69836eb1282e9d6b55828508ae42cf74e1c000b3
Aaron <agm.moya@alumno.um.edu.ar>
2023-10-04 09:19:34 -0300

    editing json flask app in azure, build

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.jsonify({'message': 'Hello World! DEPLOY!!'})
+    return flask.jsonify({'message': 'Hello World! This is a Flask App on Azure App Service'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')

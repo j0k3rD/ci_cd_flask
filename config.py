b4ab2422ddac1ec0598944126ada975d3dfa66c8
Aaron <agm.moya@alumno.um.edu.ar>
2023-10-17 20:13:29 -0300

    delete config, deploy

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-class Config(object):
-    load_dotenv()
-    CONNECTION_STRING = os.environ.get('CONNECTION_STRING')
-    OTEL_SERVICE_NAME = os.environ.get('OTEL_SERVICE_NAME')
